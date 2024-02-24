d0b79bc6802748216d3d69e5cc522c5d47dea594
Vural Sayan <vuralsayan01@gmail.com>
2024-02-24 22:19:40 +0300

    Feature statistics tamamlandı

---
+++ TraversalCoreProject/ViewComponents/Default/_StatisticsPartial.cs
@@ -0,0 +1,17 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _StatisticsPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            using var c = new Context();
+            ViewBag.TotalDestination = c.Destinations?.Count();
+            ViewBag.Guides = c.Guides?.Count();
+            ViewBag.Customer = "265";
+            return View();
+        }
+    }
+}
