16286b5b685a01948c9162456fd8fbfa700f9ab2
Vural Sayan <vuralsayan01@gmail.com>
2024-02-26 00:49:49 +0300

    Tur rotalarına yorum yapma işlemi tamamlandı

---
+++ TraversalCoreProject/Controllers/CommentController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Controllers
+{
+    public class CommentController : Controller
+    {
+        CommentManager commentManager = new CommentManager(new EfCommentDal());
+
+        [HttpGet]
+        public PartialViewResult AddComment(int id)
+        {
+            ViewBag.i = id; 
+            return PartialView();
+        }
+
+        [HttpPost]
+        public IActionResult AddComment(Comment p)
+        {
+            p.CommentDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            p.CommentStatus = true;
+            var values = p.DestinationID;
+            commentManager.TAdd(p);
+            return RedirectToAction("DestinationDetails", "Destination", new { id = values });
+        }
+    }
+}
