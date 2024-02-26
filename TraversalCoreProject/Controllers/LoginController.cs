720fd28d330c31165853cd790347bc8919a4f9f2
Vural Sayan <vuralsayan01@gmail.com>
2024-02-26 19:55:24 +0300

    Kullanıcı login işlemi tamamlandı

--- TraversalCoreProject/Controllers/LoginController.cs
+++ TraversalCoreProject/Controllers/LoginController.cs
@@ -10,10 +10,12 @@ namespace TraversalCoreProject.Controllers
     public class LoginController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-        public LoginController(UserManager<AppUser> userManager)
+        public LoginController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         [HttpGet]
@@ -57,6 +59,26 @@ namespace TraversalCoreProject.Controllers
         public IActionResult SignIn()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignIn(UserSignInViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(p.Username, p.Password, false, true);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Destination");
+                }
+                else
+                {
+                    return RedirectToAction("SignIn");  
+                }
+            }
+
+            return View(p);
         }
     }
 }
---
+++ TraversalCoreProject/Models/UserSignInViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProject.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage = "Email alanı boş geçilemez")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Şifre alanı boş geçilemez")]
+        public string? Password { get; set; }
+    }
+}
