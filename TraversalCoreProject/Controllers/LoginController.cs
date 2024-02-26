00065a67646540e69736b0439e078dafe89e6d8d
Vural Sayan <vuralsayan01@gmail.com>
2024-02-26 08:09:49 +0300

    Register Identity validasyonları yapıldı

--- TraversalCoreProject/Controllers/LoginController.cs
+++ TraversalCoreProject/Controllers/LoginController.cs
@@ -1,22 +1,56 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using TraversalCoreProject.Models;
 
 namespace TraversalCoreProject.Controllers
 {
     [AllowAnonymous]
     public class LoginController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public LoginController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [HttpGet]
         public IActionResult SignUp()
         {
             return View();
         }
 
-        //[HttpPost]
-        //public IActionResult SignUp()
-        //{
-        //    return View();
-        //}
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserRegisterViewModel p)
+        {
+            AppUser appUser = new AppUser
+            {
+                Name = p.Name,
+                Surname = p.Surname,
+                UserName = p.Username,
+                PhoneNumber = p.PhoneNumber,
+                Email = p.Mail
+            };
+            if (p.Password != null && p.ConfirmPassword != null && p.Password == p.ConfirmPassword)
+            {
+                var result = await _userManager.CreateAsync(appUser, p.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("SignIn");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(p);
+        }
 
 
         [HttpGet]
---
+++ TraversalCoreProject/Models/UserRegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProject.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage ="Lütfen adınızı giriniz")]  
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Lütfen soyadınızı giriniz")]
+        public string? Surname { get; set; }
+
+        [Required(ErrorMessage = "Lütfen mail adresinizi giriniz")]
+        public string? Mail { get; set; }
+
+        [Required(ErrorMessage = "Lütfen kullanıcı adınızı giriniz")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Lütfen telefon numaranızı giriniz")]
+        public string? PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifrenizi giriniz")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifrenizi tekrar giriniz")]
+        [Compare("Password", ErrorMessage = "Şifreler uyuşmuyor")]
+        public string? ConfirmPassword { get; set; }
+    }
+}
