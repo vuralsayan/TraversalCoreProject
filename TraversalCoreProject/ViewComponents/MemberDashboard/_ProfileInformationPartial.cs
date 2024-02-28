e9285951f9868f30f6251de2d1a939f189a5643c
Vural Sayan <vuralsayan01@gmail.com>
2024-02-28 15:55:12 +0300

    Member dashboard bilgileri listelendi

---
+++ TraversalCoreProject/ViewComponents/MemberDashboard/_ProfileInformationPartial.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.MemberDashboard
+{
+    public class _ProfileInformationPartial : ViewComponent
+    {
+        public readonly UserManager<AppUser> _userManager;
+
+        public _ProfileInformationPartial(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity?.Name);
+            ViewBag.memberName = values.Name + " " + values.Surname;
+            ViewBag.phoneNumber = values.PhoneNumber;
+            ViewBag.mail = values.Email;    
+            return View();
+        }
+    }
+}
+
