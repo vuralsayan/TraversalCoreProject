182068ce7e37fa623f7b3c0ac1041a6f42c1d6e9
Vural Sayan <vuralsayan01@gmail.com>
2024-03-01 01:00:53 +0300

    Admin Destination işlemleri tamamlandı

--- TraversalCoreProject/Areas/Admin/Controllers/DestinationController.cs
+++ TraversalCoreProject/Areas/Admin/Controllers/DestinationController.cs
@@ -1,13 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class DestinationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
         public IActionResult Index()
         {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddDestination()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddDestination(Destination destination)
+        {
+            destinationManager.TAdd(destination);
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult DeleteDestination(int id)
+        {
+            var value = destinationManager.TGetByID(id);
+            destinationManager.TDelete(value);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateDestination(int id)
+        {
+            var value = destinationManager.TGetByID(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateDestination(Destination destination)
+        {
+            destinationManager.TUpdate(destination);
+            return RedirectToAction("Index", "Destination", new { area = "Admin" });
         }
     }
 }
